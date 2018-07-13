c32f5a8f93a7c69604082a0c9fb2e008273506d1
reneewhite <reneilwesefora@gmail.com>
2018-07-13 15:55:13 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private flashlight: Flashlight) {
 
   }
+
+  onFlashOn(){
+    this.flashlight.switchOn();
+  }
+
+  onFlashOff(){
+    this.flashlight.switchOff();
+  }
+
 
 }
